e050f7489335a9d9f248dd62d616a313944a9fc5
Dean Isseyegh <dean.isseyegh@secretescapes.com>
2015-10-09 14:14:49 +0100

    Made gradle proj executable with "gradle run"

--- src/main/groovy/com/secretescapes/jirereleasenotesauto/MainApp.groovy
+++ src/main/groovy/com/secretescapes/jirereleasenotesauto/MainApp.groovy
@@ -6,15 +6,15 @@ package com.secretescapes.jirereleasenotesauto
  */
 class MainApp {
 
-	public static void main(String... args) {
+	public static void main(String[] args) {
 		if (args.size() != 2) {
 			throw new IllegalArgumentException("Must enter just two arguments - Your encoded64 username and password, the release version number. e.g. jar ZGVWltMQ= 2.71")
 		} else {
 			JiraReleaseNotes jiraReleaseNotes = new JiraReleaseNotes()
 			ReleaseNotes releaseNotes = jiraReleaseNotes.getFixNotes(args[0], args[1])
 			String notesAsHTML = jiraReleaseNotes.formatAsHtml(releaseNotes)
-			println notesAsHTML
-			notesAsHTML
+			File file = new File('.', 'release-notes.html')
+			file << notesAsHTML
 		}
 	}
 }
